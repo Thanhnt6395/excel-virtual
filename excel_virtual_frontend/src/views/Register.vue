146657731645dca53879d5215e9a1698a4836fce
thanhnt <thanh.nguyen@codelovers.vn>
2023-08-21 14:21:54 +0700

    modify register page

--- excel_virtual_frontend/src/views/Register.vue
+++ excel_virtual_frontend/src/views/Register.vue
@@ -5,20 +5,40 @@
         <h1 class=" text-blue-950 font-bold uppercase mb-2 text-center text-3xl">Register</h1>
         <h3 class=" text-blue-950 font-light mb-7 text-center text-xl">Create your account</h3>
         <div class=" grid grid-cols-2 gap-x-3 gap-y-5">
-          <div class=" col-span-2">
-            <input type="text" name="" id="email" placeholder="Email" class=" border border-gray-200 rounded w-full h-[3rem] px-3 py-2">
+          <div class=" col-span-2 relative">
+            <label for="email" :class="[showLabel.email ? 'visible absolute top-[-0.5rem] left-[1rem] bg-white px-1': 'invisible']">Email</label>
+            <input 
+              type="text" name="" id="email" placeholder="Email" 
+              class=" border border-gray-200 rounded w-full h-[3rem] px-3 py-2" 
+              v-model="form['email']" @input="onInput">
           </div>
           <div class=" col-span-1">
-            <input type="text" name="" id="first_name" placeholder="First Name" class=" border border-gray-200 rounded w-full h-[3rem] px-3 py-2">
+            <label for="first_name" :class="[showLabel.first_name ? 'visible absolute top-[-0.5rem] left-[1rem] bg-white px-1': 'invisible']">First Name</label>
+            <input 
+              type="text" name="" id="first_name" placeholder="First Name" 
+              class=" border border-gray-200 rounded w-full h-[3rem] px-3 py-2" 
+              v-model="form['first_name']" @input="onInput">
           </div>
           <div class=" col-span-1">
-            <input type="text" name="" id="last_name" placeholder="Last Name" class=" border border-gray-200 rounded w-full h-[3rem] px-3 py-2">
+            <label for="last_name" :class="[showLabel.last_name ? 'visible absolute top-[-0.5rem] left-[1rem] bg-white px-1': 'invisible']">Last Name</label>
+            <input 
+              type="text" name="" id="last_name" placeholder="Last Name" 
+              class=" border border-gray-200 rounded w-full h-[3rem] px-3 py-2" 
+              v-model="form['last_name']" @input="onInput">
           </div>
           <div class=" col-span-2">
-            <input type="text" name="" id="password" placeholder="Password" class=" border border-gray-200 rounded w-full h-[3rem] px-3 py-2">
+            <label for="password" :class="[showLabel.password ? 'visible absolute top-[-0.5rem] left-[1rem] bg-white px-1': 'invisible']">Password</label>
+            <input 
+              type="password" name="" id="password" placeholder="Password" 
+              class=" border border-gray-200 rounded w-full h-[3rem] px-3 py-2" 
+              v-model="form['password']" @input="onInput">
           </div>
           <div class=" col-span-2">
-            <input type="text" name="" id="repeat_password" placeholder="Repeat Password" class=" border border-gray-200 rounded w-full h-[3rem] px-3 py-2">
+            <label for="repeat_password" :class="[showLabel.repeat_password ? 'visible absolute top-[-0.5rem] left-[1rem] bg-white px-1': 'invisible']">Repeat Password</label>
+            <input 
+              type="password" name="" id="repeat_password" placeholder="Repeat Password" 
+              class=" border border-gray-200 rounded w-full h-[3rem] px-3 py-2" 
+              v-model="form['repeat_password']" @input="onInput">
           </div>
           <div class=" col-span-1">
             <input type="checkbox" name="" id="show-password" class=" mr-3">
@@ -37,6 +57,22 @@
 </template>
 
 <script setup>
+import { ref, computed } from 'vue';
+
+const form = ref({})
+const showLabel = ref({
+  email: false,
+  first_name: false,
+  last_name: false,
+  password: false,
+  repeat_password: false
+})
+
+function onInput(e) {
+  let id = e.target.id
+  if (!form.value[id] || form.value[id] == '') showLabel.value[id] = false
+  else showLabel.value[id] = true
+}
 
 </script>
 
